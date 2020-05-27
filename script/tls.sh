0035b775c02b367b25423d4450da6f074b61a8c0
John Martin <johnjosephmartin@icloud.com>
2020-05-27 16:47:49 -0400

    only use root cert (#11)

--- script/tls.sh
+++ script/tls.sh
@@ -2,6 +2,7 @@
 set -eu
 
 ca_pem_file="/kafka/ca.pem"
+ca_tmp_file="/tmp/ca.pem"
 
 IFS=','
 read -ra SERVERS <<< "${BOOTSTRAP_SERVERS}"
@@ -9,7 +10,9 @@ read -ra SERVERS <<< "${BOOTSTRAP_SERVERS}"
 echo "Generating certs from ${SERVERS[0]}"
 
 openssl s_client -showcerts -connect ${SERVERS[0]} \
-    2>/dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > ${ca_pem_file}
+    2>/dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > ${ca_tmp_file}
+
+sed -n -e "$(echo $(grep -n CERT ${ca_tmp_file} | cut -d: -f1 | tail -2) | xargs | sed 's/ /,/')p" ${ca_tmp_file} > ${ca_pem_file}
 
 openssl pkcs12 -export -password pass:${CONNECT_SSL_KEYSTORE_PASSWORD} -out ${KEYSTORE_LOCATION} \
     -inkey /ssl/kafka-client/tls.key -certfile ${ca_pem_file} -in /ssl/kafka-client/tls.crt \
