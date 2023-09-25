f1e7e6c5f82ac6b74809ef52769963a90ad0d3ef
klam-shop <78921373+klam-shop@users.noreply.github.com>
2023-09-25 17:15:01 -0400

    Add toggle for enabling and disabling profiler  (#216)
    * Add toggle for profiler
    
    * typo
    
    * handle when flag is not present

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -4,4 +4,8 @@ set -euo
 mkdir /app/ssl
 /usr/local/create_truststore.sh
 
+if [[ -n "${PROFILER_ENABLED:-}" && "$PROFILER_ENABLED" == "true" ]]; then
+  export KAFKA_OPTS="$KAFKA_OPTS $PROFILER_CONFIGS"
+fi
+
 exec /docker-entrypoint.sh start
