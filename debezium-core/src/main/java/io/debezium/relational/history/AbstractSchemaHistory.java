1980c980bf6ccd264696567d7518e5ca3fbcbee2
Ahmed Bayraktar <ahmed.bayraktar@shopify.com>
2025-04-03 11:42:28 -0400

    Remove DDL filter log

--- debezium-core/src/main/java/io/debezium/relational/history/AbstractSchemaHistory.java
+++ debezium-core/src/main/java/io/debezium/relational/history/AbstractSchemaHistory.java
@@ -131,8 +131,8 @@ public abstract class AbstractSchemaHistory implements SchemaHistory {
                         ddlParser.setCurrentSchema(recovered.schemaName()); // may be null
                     }
                     if (ddlFilter.test(ddl)) {
-                        logger.info("a DDL '{}' was filtered out of processing by regular expression '{}'", ddl,
-                                config.getString(DDL_FILTER));
+                        // logger.info("a DDL '{}' was filtered out of processing by regular expression '{}'", ddl,
+                        //         config.getString(DDL_FILTER));
                         return;
                     }
                     try {
